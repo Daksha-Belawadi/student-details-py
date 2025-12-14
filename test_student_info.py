9ace0bb09d542b3d56fe7cb83ba295b119418c41
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-12-14 14:11:29 +0530

    Update test_student_info.py

--- test_student_info.py
+++ test_student_info.py
@@ -2,7 +2,7 @@ from student_info import student_info
 
 def test_student_info():
     result = student_info("Daksha", 186, "BCA", 98)
-    expected = "Student Name: Ananya, Roll No: 101, Course: BCA, Marks: 88"
+    expected = "Student Name: Daksha, Roll No: 186, Course: BCA, Marks: 98"
 
     print(result)   
 
