3650fc6a5ea6b82aa1a820bf414a06c79eb540a0
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-12-14 14:09:34 +0530

    Update test_student_info.py

--- test_student_info.py
+++ test_student_info.py
@@ -2,5 +2,8 @@ from student_info import student_info
 
 def test_student_info():
     result = student_info("Daksha", 186, "BCA", 98)
-    expected = "Student Name: Daksha, Roll No: 186, Course: BCA, Marks: 98"
+    expected = "Student Name: Ananya, Roll No: 101, Course: BCA, Marks: 88"
+
+    print(result)   
+
     assert result == expected
